0028fdf7f4f56062ca478f88dd369ef2a9667960
kamil.oberaj <kamil.oberaj@gmail.com>
2024-09-19 19:19:05 +0200

    initial

---
+++ db/sqlc/account_test.go
@@ -0,0 +1,92 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"github.com/go-faker/faker/v4"
+	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
+)
+
+func TestCreateAccount(t *testing.T) {
+	createRandomAccount(t)
+}
+
+func TestGetAccount(t *testing.T) {
+	acc1 := createRandomAccount(t)
+	acc2, err := testQueries.GetAccount(context.Background(), acc1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, acc2)
+	require.Equal(t, acc1.ID, acc2.ID)
+	require.Equal(t, acc1.Owner, acc2.Owner)
+	require.Equal(t, acc1.Balance, acc2.Balance)
+	require.Equal(t, acc1.Currency, acc2.Currency)
+	require.WithinDuration(t, acc1.CreatedAt, acc2.CreatedAt, time.Second)
+}
+
+func TestUpdateAccount(t *testing.T) {
+	acc := createRandomAccount(t)
+
+	arg := UpdateAccountParams{
+		ID:      acc.ID,
+		Balance: faker.UnixTime(),
+	}
+
+	acc2, err := testQueries.UpdateAccount(context.Background(), arg)
+	require.NoError(t, err)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, acc2)
+	require.Equal(t, acc.ID, acc2.ID)
+	require.Equal(t, acc.Owner, acc2.Owner)
+	require.Equal(t, acc2.Balance, arg.Balance)
+	require.Equal(t, acc.Currency, acc2.Currency)
+}
+
+func TestDeleteAccount(t *testing.T) {
+	acc := createRandomAccount(t)
+	err := testQueries.DeleteAccount(context.Background(), acc.ID)
+	require.NoError(t, err)
+
+	acc2, err := testQueries.GetAccount(context.Background(), acc.ID)
+	require.Error(t, err)
+	require.Empty(t, acc2)
+	require.EqualError(t, err, sql.ErrNoRows.Error())
+}
+
+func TestListAccounts(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		createRandomAccount(t)
+	}
+
+	accounts, err := testQueries.ListAccounts(context.Background(), ListAccountsParams{
+		Limit:  5,
+		Offset: 5,
+	})
+
+	require.NoError(t, err)
+
+	for _, acc := range accounts {
+		require.NotEmpty(t, acc)
+	}
+}
+
+func createRandomAccount(t *testing.T) Account {
+	arg := CreateAccountParams{
+		Owner:    faker.Name(),
+		Balance:  faker.UnixTime(),
+		Currency: faker.Currency(),
+	}
+
+	account, err := testQueries.CreateAccount(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, account)
+	require.Equal(t, arg.Owner, account.Owner)
+	require.Equal(t, arg.Balance, account.Balance)
+	require.Equal(t, arg.Currency, account.Currency)
+	require.NotZero(t, account.CreatedAt)
+	require.NotZero(t, account.ID)
+
+	return account
+}
